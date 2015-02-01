b967af599cf3623648337bda0bc7bef9596d33f2
Cherwin Nooitmeer <cherwin@gmail.com>
2015-02-01 09:18:43 +0100

    Refactor to sane variables and function names

--- esxi_helper.py
+++ esxi_helper.py
@@ -10,34 +10,33 @@ for c in ['status', 'running', 'stdout']:
 
 class VirtualMachines(object):
     def __init__(self):
-        self._cmd = fbapi.run("vim-cmd vmsvc/getallvms")
+        self._allvms = fbapi.run("vim-cmd vmsvc/getallvms")
         # Split header from "getallvms" into keys
-        keys = self._cmd.splitlines()[0].lower().split()
+        attribute_names = self._allvms.splitlines()[0].lower().split()
         # ['vmid', 'name', 'file', 'guest', 'os', 'version', 'annotation']
         #
-        # 'guest' 'os' elements should be 1 element
-        keys.insert(3, keys.pop(3) + keys.pop(3))
+        # Join 'guest' and 'os' attributes
+        attribute_names.insert(3, attribute.pop(3) + attribute.pop(3))
         # ['vmid', 'name', 'file', 'guestos', 'version', 'annotation']
         #
         # Insert 'storage' key to refer to type of storage VM is located on
-        keys.insert(2, 'storage')
+        attribute_names.insert(2, 'storage')
         # ['vmid', 'name', 'storage', 'file', 'guestos', 'version', 'annotation']
-        tmplist = self._cmd.splitlines()[1:]
+        allvms_normalized = self._allvms.splitlines()[1:]
 
-        self._vmlist = self._format(keys, tmplist)
+        self._inventory = self._build_inventory(attribute_names, allvms_normalized)
 
-    def _format(self, keys, tmplist):
-        "Build list of dicts from raw list"
-        result = []
-        for i in [ zip(keys, i.split()) for i in tmplist ]:
-            result.append({ k:v for k,v in i })
-        return result
+    def _build_inventory(self, attributes_names, allvms_normalized):
+        inventory = []
+        for metadata in [ zip(attribute_names, attributes.split()) for attributes in allvms_normalized ]:
+            inventory.append({ name:attribute for name, attribute in metadata })
+        return inventory
 
     def list_vms(self):
-        dicts = {}
-        for d in self._vmlist:
-            dicts[d['name']] = d
-        return dicts
+        vm_names = {}
+        for vm in self._inventory:
+            vm_names[vm['name']] = vm
+        return vm_names
 
     def name_to_id(self, name):
         return self.list_vms()[name]['vmid']
