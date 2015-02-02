01e3638c7671b91aa173196bcde5a511f9981728
Cherwin Nooitmeer <cherwin@gmail.com>
2015-02-02 02:17:38 +0100

    Refacter attribute_names to headers

--- esxi_helper.py
+++ esxi_helper.py
@@ -4,24 +4,21 @@ import json
 class VirtualMachines(object):
     def __init__(self, vm_list):
         self.vm_list = vm_list
-        # Split header from "getvm_list" into keys
-        self.attribute_names = self.vm_list.splitlines()[0].lower().split()
-        # ['vmid', 'name', 'file', 'guest', 'os', 'version', 'annotation']
-        #
-        # Join 'guest' and 'os' attributes
-        self.attribute_names.insert(3, self.attribute_names.pop(3) + self.attribute_names.pop(3))
-        # ['vmid', 'name', 'file', 'guestos', 'version', 'annotation']
-        #
-        # Insert 'storage' key to refer to type of storage VM is located on
-        self.attribute_names.insert(2, 'storage')
-        # ['vmid', 'name', 'storage', 'file', 'guestos', 'version', 'annotation']
         self.vm_list_normalized = self.vm_list.splitlines()[1:]
+        self.headers = self._build_headers()
+        self.inventory = self._build_inventory()
 
-        self.inventory = self._build_inventory(self.attribute_names, self.vm_list_normalized)
+    def _build_headers(self):
+        headers = self.vm_list.splitlines()[0].lower().split()
+        guest   = headers.pop(3)
+        os      = headers.pop(3)
+        headers.insert(3, guest + os)
+        headers.insert(2, 'storage')
+        return headers
 
-    def _build_inventory(self, attribute_names, vm_list_normalized):
+    def _build_inventory(self):
         inventory = defaultdict(dict)
-        for metadata in [ zip(attribute_names, attributes.split()) for attributes in vm_list_normalized ]:
+        for metadata in [ zip(self.headers, attributes.split()) for attributes in self.vm_list_normalized ]:
             # use use vm name -> metadata[1][1] as the key
             inventory[metadata[1][1]] = { name:attribute for name, attribute in metadata }
         return inventory
